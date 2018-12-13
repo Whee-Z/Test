a3ea647486a9a0b1c16168cf19c08d72f700b161
Whee-Z <42569161+Whee-Z@users.noreply.github.com>
2018-12-13 23:30:19 +0800

    Add files via upload
    插入排序算法小练习

---
+++ test5-insertion_sort.py
@@ -0,0 +1,27 @@
+﻿#插入排序算法,输入数字时以逗号隔开,输入exit退出循环
+class Insertion_sort(object):
+    def __init__(self):
+        pass
+
+    def insertion_sort(self,alist):
+        n = len(alist)
+        for i in range(1,n):
+            for j in range(i,0,-1):
+                if alist[j] < alist[j-1]:
+                    alist[j],alist[j-1] = alist[j-1],alist[j]
+
+def main():
+    insertion = Insertion_sort()
+    while True:
+        alist_str = input("Please input a list:")
+        alist = []
+        if 'exit' == alist_str:
+            break
+        for i in eval(alist_str):
+            alist.append(i)    
+
+        insertion.insertion_sort(alist)
+        print(alist)
+
+if __name__ == '__main__':
+    main()
