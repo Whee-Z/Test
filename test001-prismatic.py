7f1e05a5288a0ba511c0da5f91ad6ef96c33445d
Whee-Z <42569161+Whee-Z@users.noreply.github.com>
2019-01-02 21:25:19 +0800

    打印一个棱形

---
+++ test001-prismatic.py
@@ -0,0 +1,13 @@
+﻿#打印一个棱形
+
+def prismatic(n):
+    for i in range(0, n):
+        for j in range(0, n-i):
+            print(" ", end="")
+        print('* ' * i)
+    for i in range(0, n):
+        for k in range(0, i):
+            print(" ", end="")
+        print('* ' * (n-i))
+
+prismatic(10)
