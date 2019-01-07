ce7b00bd44d281d0105616f0d978ee738ad62e5f
Whee-Z <42569161+Whee-Z@users.noreply.github.com>
2019-01-07 21:14:20 +0800

    判断是否回文

---
+++ test006-huiwen.py
@@ -0,0 +1,12 @@
+﻿#判断是否回文
+astr = input("请输入一个数字:")
+flag = True
+
+for i in range(len(astr) // 2):
+    if astr[i] != astr[-i - 1]:
+        flag = False
+        break
+if flag:
+    print("%s 是一个回文数!" %astr)
+else:
+    print("%s 不是一个回文数!" %astr)
