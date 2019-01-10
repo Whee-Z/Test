1ae290050c31b4bab4b5ccb38878e0f5a292f0e4
Whee-Z <42569161+Whee-Z@users.noreply.github.com>
2019-01-10 21:29:19 +0800

    求出质数

---
+++ test009-zhishu.py
@@ -0,0 +1,13 @@
+﻿#求出质数
+l = []
+n = int(input("请输入一个数："))
+for i in range (2,n+1):
+    flag = 0
+    for j in range (2,i):
+        if i % j == 0:
+            flag = 1
+            break
+    if flag == 0:
+        l.append(i)
+
+print(l)
