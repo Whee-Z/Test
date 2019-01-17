b90d774060a9855226be128b5016ed22c84dcf53
Whee-Z <42569161+Whee-Z@users.noreply.github.com>
2019-01-17 18:27:11 +0800

    字符串排序

---
+++ test015-str-sort.py
@@ -0,0 +1,11 @@
+﻿#字符串排序
+str1 = input('请输入排序的第一个字符串:')
+str2 = input('请输入排序的第二个字符串:')
+str3 = input('请输入排序的第三个字符串:')
+print("排序前：",str1, str2, str3)
+
+if str1 > str2: str1, str2 = str2, str1
+if str1 > str3: str1, str3 = str3, str1
+if str2 > str3: str2, str3 = str3, str2
+
+print("排序后：",str1, str2, str3)
